37ae2116a7ae238ee9fdf1b73eee949b4abb025c
Ahmed Sameh <a10101010h@gmail.com>
2026-04-17 16:20:32 +0200

    13. roman to integer
    Need optimization

---
+++ LeetCode/easy/Roman to integer.py
@@ -0,0 +1,19 @@
+#need refactoring
+#13.roman to integer
+#https://leetcode.com/problems/roman-to-integer/description/
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {'I': 1,'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000, "IV": 4, "IX": 9, "XL": 40, "XC": 90,"CD": 400, "CM": 900}
+
+        i = 0
+        number = 0
+
+        while i < len(s):
+            if (i < len(s) - 1) and ((s[i] + s[i + 1]) in values):
+                number += values[(s[i] + s[i + 1])]
+                i += 2
+                continue
+            number += values[s[i]]
+            i += 1
+
+        return number
